557c4f6a4f1e7108c486c4320542cbaf6a81d2c4
DuaBilalli <duabilalli13@gmail.com>
2026-03-26 09:21:41 +0100

    added footer and updated header with new styles and fonts

---
+++ php/footer.php
@@ -0,0 +1,60 @@
+
+
+
+    <?php
+ include('header.php')
+    ?>
+    
+    <footer class="footer-48201">
+      
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 pr-md-5">
+            <a href="#" class="footer-site-logo d-block mb-4">EchoFest</a>
+            <img src="../images/logo2.png" height="100px">
+          </div>
+          <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Gallery</a></li>
+              <li><a href="#">Tickets</a></li>
+              <li><a href="#">Line Up</a></li>
+            </ul>
+          </div>
+          <!-- <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li><a href="#">Clients</a></li>
+              <li><a href="#">Team</a></li>
+              <li><a href="#">Career</a></li>
+              <li><a href="#">Testimonials</a></li>
+              <li><a href="#">Journal</a></li>
+            </ul>
+          </div> -->
+          <div class="col-md">
+            <ul class="list-unstyled nav-links">
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Terms &amp; Conditions</a></li>
+              <li><a href="#">Cookies</a></li>
+            </ul>
+          </div>
+        </div> 
+
+        <div class="row ">
+          <div class="col-12 text-center">
+            <div class="copyright mt-5 pt-5">
+              <p><small>&copy; 2026 All Rights Reserved.</small></p>
+            </div>
+          </div>
+        </div> 
+      </div>
+      
+    </footer>
+
+    
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/header.js"></script>
+  </body>
+</html>
