890dde0266c4c2d62682cf37b119c6bbb7de03b4
sarak1234 <sarakycyku123@gmail.com>
2026-04-20 01:53:04 +0200

    implementimi i logijkes per login

---
+++ logic/login_logic.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include "../data/users.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    // kontrollo a ekziston useri
+    if (isset($users[$username])) {
+
+        // kontrollo password
+        if ($users[$username]['password'] === $password) {
+
+            // login sukses
+            $_SESSION['username'] = $username;
+            $_SESSION['role'] = $users[$username]['role'];
+
+            if ($users[$username]['role'] == "admin") {
+                header("Location: ../pages/admin.php");
+                exit;
+            } else {
+                header("Location: ../pages/home.php");
+                exit;
+            }
+
+        } else {
+            echo "Password gabim!";
+        }
+
+    } else {
+        echo "Useri nuk ekziston!";
+    }
+}
+?>
