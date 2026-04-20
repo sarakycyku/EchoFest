8d5e3075aead86759cee97128629a3b1219f87dd
DuaBilalli <duabilalli13@gmail.com>
2026-04-21 00:13:15 +0200

    Add ticket information display in tickets.php and create tickets.css for styling

--- pages/tickets.php
+++ pages/tickets.php
@@ -1,10 +1,52 @@
 <?php
 include '../includes/header.php';
+
+$locations = [
+    'xk' => [
+        'flag'=>'XK',
+        'country'=>'Kosovo',
+        'city'=>'Pristina',
+        'dates'=>'July 15-17, 2026',
+        'venue'=>'Pristina National Stadium, Rr. Agim Ramadani, Pristina 10000'
+    ],
+    'al' => [
+        'flag'=>'AL',
+        'country'=>'Albania',
+        'city'=>'Durrës',
+        'dates'=>'August 5-7, 2026',
+        'venue'=>'Arena Kombëtare, Durrës, Albania'
+    ],
+];
+
+//lexon lokacionin nga GET, default xk
+$selected = isset($_GET['loc']) && array_key_exists($_GET['loc'], $locations) ? $_GET['loc'] :'xk'; 
+$event = $locations[$selected];
+
 ?>
 
 <title>Tickets</title>    
 <link rel="stylesheet" href="../assets/css/style.css">
 <link rel="stylesheet" href="../assets/css/tickets.css">
+
+<section class="hero">
+
+    <div class="hero-info">
+      <div class="info-block">
+        <span class="info-label">Festival Dates</span>
+        <span class="info-value">
+          <?= $event['dates'] ?>
+        </span>
+      </div>
+      <div class="info-block">
+        <span class="info-label">Location</span>
+        <span class="info-value">
+          <?= $event['venue'] ?>
+        </span>
+      </div>
+    </div>
+  </div>
+
+</section>
 
 <?php
 include '../includes/footer.php';
