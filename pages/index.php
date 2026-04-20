b58506b29a76f0324933f9295bf55b42b69d0b4d
sarak1234 <sarakycyku123@gmail.com>
2026-04-21 00:07:50 +0200

    krijimi i faqes se index

---
+++ pages/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" type="image/x-icon" href="../assets/images/logo2-pabg.png">
+  <title>ECHOFEST</title>
+  <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=DM+Sans:wght@300;400;500&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="../assets/css/index.css" />
+  </style>
+</head>
+<body>
+
+<div class="bg"></div>
+<div class="grid"></div>
+
+<div class="particles" id="particles"></div>
+
+<div class="wrapper">
+  <p class="eyebrow">Official Portal &mdash; 2026 Edition</p>
+  <h1 class="logo">
+    ECHO
+    <span>FEST</span>
+  </h1>
+  <p class="tagline">Where sound becomes memory &nbsp;&bull;&nbsp; July 18&ndash;21 &bull;&nbsp; Open Air Arena</p>
+  <div class="actions">
+    <a href="login.php" class="btn btn-login">Log In</a>
+    <a href="signup.php" class="btn btn-signup">Sign Up</a>
+  </div>
+</div>
+
+
+
+
+</body>
+</html>
