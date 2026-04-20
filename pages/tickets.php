0da92cae9010f6496473c03512ca3561d663b8b3
DuaBilalli <duabilalli13@gmail.com>
2026-04-20 23:42:33 +0200

    Add tickets.php file with header and footer includes

---
+++ pages/tickets.php
@@ -0,0 +1,11 @@
+<?php
+include '../includes/header.php';
+?>
+
+<title>Tickets</title>    
+<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="../assets/css/tickets.css">
+
+<?php
+include '../includes/footer.php';
+?>
