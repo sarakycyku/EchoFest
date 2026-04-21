1516c7090a329f9e529965be203a7ec196000244
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-21 22:36:45 +0200

    initial html structure

---
+++ pages/profile.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Profile</title>
+  <link rel="stylesheet" href="../css/profile.css">
+</head>
+
+<body>
+
+</body>
+</html>
