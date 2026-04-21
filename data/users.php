dc3f237fcd356d539aebdcc1107afa958c6221d3
sarak1234 <sarakycyku123@gmail.com>
2026-04-21 21:35:32 +0200

    ndryshime ne te dhena te users

--- data/users.php
+++ data/users.php
@@ -1,20 +1,56 @@
-<?
+<?php
+
 $users = [
-    "admin" => [
-        "password" => "Admin123!",
-        "role" => "admin"
+
+    "admin1" => [
+        "password" => "John12!",
+        "role" => "admin",
+        "first_name" => "John",
+        "last_name" => "Doe",
+        "email" => "admin1@gmail.com",
+        "phone" => "044123456",
+        "age" => 25
     ],
+
+    "admin2" => [
+        "password" => "Smith3#",
+        "role" => "admin",
+        "first_name" => "Jane",
+        "last_name" => "Smith",
+        "email" => "admin2@echofest.com",
+        "phone" => "052229537",
+        "age" => 31
+    ],
+
     "user1" => [
-        "password" => "User123!",
-        "role" => "user"
+        "password" => "@user12?",
+        "role" => "user",
+        "first_name" => "Michael",
+        "last_name" => "Brown",
+        "email" => "user1@gmail.com",
+        "phone" => "03289326166",
+        "age" => 19
     ],
+
     "user2" => [
-        "password" => "User456!",
-        "role" => "user"
+        "password" => "JEmiliy12.",
+        "role" => "user",
+        "first_name" => "Emily",
+        "last_name" => "Johnson",
+        "email" => "user2@gmail.com",
+        "phone" => "034271796",
+        "age" => 55
     ],
+
     "user3" => [
-        "password" => "User789!",
-        "role" => "user"
+        "password" => "D4vid.",
+        "role" => "user",
+        "first_name" => "David",
+        "last_name" => "Wilson",
+        "email" => "user3@gmail.com",
+        "phone" => "05444166693",
+        "age" => 40
     ]
+
 ];
 ?>
