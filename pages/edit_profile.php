9f61bd55df57c9f5f766ec55b954f52e6bc2b493
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-23 00:18:01 +0200

    shto kontrollin e qasjes dhe marrjen e te dhenave

--- pages/edit_profile.php
+++ pages/edit_profile.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+if (!isset($_SESSION['username'])) {
+    header("Location: ../pages/login.php");
+    exit;
+}
+include "../data/users.php";
+$username = $_SESSION['username'];
+$data = $users[$username];
+?>
+
 
 <!DOCTYPE html>
 
