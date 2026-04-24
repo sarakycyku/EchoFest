52db0d71badc597f86698f3496b07e383b16cedf
DuaBilalli <duabilalli13@gmail.com>
2026-04-25 01:23:57 +0200

    Permiresimi i form ne purchase me vlera te mbushura paraprakisht dhe placeholders per te dhenat e userit

--- pages/purchase.php
+++ pages/purchase.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // fillon sessioni
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -11,17 +11,17 @@ if (!isset($_SESSION['username'])) {
 }
 
 //merr te dhenat e userit nga user.php
-include "../data/users.php"; 
+include "../data/users.php";
 
-$username  = $_SESSION['username'];
-$data      = $users[$username];
+$username = $_SESSION['username'];
+$data = $users[$username];
 
 $firstName = $data['first_name'] ?? '';
-$lastName  = $data['last_name']  ?? '';
-$email     = $data['email']      ?? '';
-$phone     = $data['phone']      ?? '';
+$lastName = $data['last_name'] ?? '';
+$email = $data['email'] ?? '';
+$phone = $data['phone'] ?? '';
 
-include '../includes/header.php'; 
+include '../includes/header.php';
 ?>
 
 <link rel="stylesheet" href="../assets/css/purchase.css">
@@ -29,7 +29,49 @@ include '../includes/header.php';
 <h1 class="page-title">Complete Your Purchase</h1>
 
 <form method="POST" action="purchase_logic.php">
-<div class="purchase-layout"></div>
+    <div class="purchase-layout">
+
+        <div class="forms-col">
+
+            <div class="panel">
+                <div class="panel-title">Personal Information</div>
+                <div class="form-grid">
+                    <div class="field">
+                        <label>First Name <span class="req">*</span></label>
+                        <input type="text" name="first_name"
+                            value="<?=$firstName?>"
+                            class="<?= $firstName ? 'prefilled' : '' ?>"
+                            placeholder="First Name"
+                            <?= $firstName ? 'readonly' : 'required' ?>>
+                    </div>
+                    <div class="field">
+                        <label>Last Name <span class="req">*</span></label>
+                        <input type="text" name="last_name"
+                            value="<?=$lastName?>"
+                            class="<?=$lastName ? 'prefilled' : '' ?>"
+                            placeholder="Last Name"
+                            <?= $lastName ? 'readonly' : 'required' ?>>
+                    </div>
+                    <div class="field">
+                        <label>Email <span class="req">*</span></label>
+                        <input type="email" name="email"
+                            value="<?=$email?>"
+                            class="<?=$email ? 'prefilled' : '' ?>"
+                            placeholder="john@example.com"
+                            <?= $email ? 'readonly' : 'required' ?>>
+                    </div>
+                    <div class="field">
+                        <label>Phone Number <span class="req">*</span></label>
+                        <input type="tel" name="phone"
+                            value="<?= $phone?>"
+                            class="<?=$phone ? 'prefilled' : '' ?>"
+                            placeholder="+383 44 000 000"
+                            <?= $phone ? 'readonly' : 'required' ?>>
+                    </div>
+                </div>
+            </div>
+
+        </div>
 </form>
 
 <?php include '../includes/footer.php'; ?>
