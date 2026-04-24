58541489267649664efeca1b5dfc6e42886b8fbb
DuaBilalli <duabilalli13@gmail.com>
2026-04-25 01:05:36 +0200

    Refactor purchase.php to handle session management and user data retrieval

--- pages/purchase.php
+++ pages/purchase.php
@@ -1,4 +1,29 @@
-<?php include '../includes/header.php'; ?>
+<?php 
+// fillon sessioni
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['username'])) {
+    $_SESSION['redirect_after_login'] = 'purchase.php?' . http_build_query($_GET);
+    header("Location: ../pages/login.php");
+    exit;
+}
+
+//merr te dhenat e userit nga user.php
+include "../data/users.php"; 
+
+$username  = $_SESSION['username'];
+$data      = $users[$username];
+
+$firstName = $data['first_name'] ?? '';
+$lastName  = $data['last_name']  ?? '';
+$email     = $data['email']      ?? '';
+$phone     = $data['phone']      ?? '';
+
+include '../includes/header.php'; 
+?>
+
 <link rel="stylesheet" href="../assets/css/purchase.css">
 
 <h1 class="page-title">Complete Your Purchase</h1>
