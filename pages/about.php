6cf97e249002b627b1b106384e8b5ba33b5a179b
Pranvera08 <pranvera.gashi18@student.uni-pr.edu>
2026-04-24 21:34:39 +0200

    krijimi i struktures baze te faqes about us me header, footer dhe hero section

---
+++ pages/about.php
@@ -0,0 +1,15 @@
+<?php
+include '../includes/header.php';
+?>
+
+<link rel="stylesheet" href="../assets/css/about.css">
+
+<section class="about-hero">
+    <div class="overlay"></div>
+    <div class="hero-content">
+        <h1>ABOUT US</h1>
+        <p>Where sound becomes memory</p>
+    </div>
+</section>
+
+<?php include '../includes/footer.php'; ?>
