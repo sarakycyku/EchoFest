19560142335bef016c4b1672f7c92854f15ee54e
DuaBilalli <duabilalli13@gmail.com>
2026-04-25 01:48:32 +0200

    Shtimi i funksioneve per formatimin e card input ne purchase

--- pages/purchase.php
+++ pages/purchase.php
@@ -113,4 +113,17 @@ include '../includes/header.php';
         </div>
 </form>
 
+<script>
+    function formatCard(input) {
+        let v = input.value.replace(/\D/g, '').substring(0, 16);
+        input.value = v.replace(/(.{4})/g, '$1 ').trim();
+    }
+
+    function formatExpiry(input) {
+        let v = input.value.replace(/\D/g, '').substring(0, 4);
+        if (v.length >= 3) v = v.slice(0, 2) + '/' + v.slice(2);
+        input.value = v;
+    }
+</script>
+
 <?php include '../includes/footer.php'; ?>
