7d16cc67da925014517d0030eaaf04a496e90132
Pranvera08 <pranvera.gashi18@student.uni-pr.edu>
2026-04-25 22:12:39 +0200

    shtimi i marrjes se inputeve nga GET dhe validimit me regex

--- pages/events.php
+++ pages/events.php
@@ -7,6 +7,23 @@ $locations = [
     "xk" => ["country" => "Kosovo", "city" => "Pristina", "dates" => "July 15-17, 2026", "venue" => "Pristina National Stadium"],
     "al" => ["country" => "Albania", "city" => "Durrës", "dates" => "August 5-7, 2026", "venue" => "Arena Kombëtare, Durrës"]
 ];
+
+$selectedLocation = $_GET['location'] ?? "xk";
+$search = trim($_GET['search'] ?? "");
+$category = $_GET['category'] ?? "all";
+$sort = $_GET['sort'] ?? "date";
+
+if (!array_key_exists($selectedLocation, $locations)) {
+    $selectedLocation = "xk";
+}
+
+$error = "";
+
+if ($search !== "" && !preg_match("/^[a-zA-Z0-9\s\-&,.']*$/", $search)) {
+    $error = "Invalid characters in search.";
+    $search = "";
+}
+
 ?>
 
 <title>Events</title>
