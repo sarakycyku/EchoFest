ccd30c3e6fc060ef9e0c0c131efdb96ffbf0ab88
Rumesa <rumesa2005@gmail.com>
2026-04-26 01:51:11 +0200

    added new singers to lineup

--- pages/lineup.php
+++ pages/lineup.php
@@ -9,13 +9,28 @@ $events = [
     ["artist"=>"DJ Snake","stage"=>"Main Stage","day"=>"Friday","image"=>"../assets/images/dj_snake.png","hits"=>["Taki Taki","Lean On","Let Me Love You"]],
     ["artist"=>"Martin Garrix","stage"=>"EDM Stage","day"=>"Sunday","image"=>"../assets/images/martin_garrix.png","hits"=>["Animals","Scared to Be Lonely","In the Name of Love"]],
     ["artist"=>"Rita Ora","stage"=>"Pop Stage","day"=>"Friday","image"=>"../assets/images/rita_ora.png","hits"=>["Anywhere","Let You Love Me","Your Song"]],
-    ["artist"=>"The Weeknd","stage"=>"Main Stage","day"=>"Saturday","image"=>"../assets/images/the_weeknd.png","hits"=>["Blinding Lights","Starboy","Save Your Tears"]],
+    ["artist"=>"The Weeknd","stage"=>"Main Stage","day"=>"Saturday","image"=>"../assets/images/weeknd.png","hits"=>["Blinding Lights","Starboy","Save Your Tears"]],
     ["artist"=>"Calvin Harris","stage"=>"EDM Stage","day"=>"Sunday","image"=>"../assets/images/calvin_harris.png","hits"=>["Summer","One Kiss","Feel So Close"]],
     ["artist"=>"Billie Eilish","stage"=>"Main Stage","day"=>"Friday","image"=>"../assets/images/billie_eilish.png","hits"=>["Bad Guy","Ocean Eyes","Happier Than Ever"]],
     ["artist"=>"Ed Sheeran","stage"=>"Acoustic Stage","day"=>"Saturday","image"=>"../assets/images/ed_sheeran.png","hits"=>["Shape of You","Perfect","Photograph"]],
     ["artist"=>"David Guetta","stage"=>"EDM Stage","day"=>"Friday","image"=>"../assets/images/david_guetta.png","hits"=>["Titanium","Play Hard","Without You"]],
-    ["artist"=>"Sia","stage"=>"Pop Stage","day"=>"Sunday","image"=>"../assets/images/sia.png","hits"=>["Chandelier","Cheap Thrills","Elastic Heart"]],
-];
+       ["artist"=>"Sia","stage"=>"Pop Stage","day"=>"Sunday","image"=>"../assets/images/sia.png","hits"=>["Chandelier","Cheap Thrills","Elastic Heart"]
+   ],
+ ["artist"=>"Rihanna","stage"=>"Main Stage","day"=>"Friday","image"=>"../assets/images/rihanna.png","hits"=>["Diamonds","Umbrella","We Found Love"]],
+
+     ["artist"=>"Drake","stage"=>"Main Stage","day"=>"Saturday","image"=>"../assets/images/drake.png","hits"=>["God's Plan","Hotline Bling","One Dance"]],
+
+     ["artist"=>"Taylor Swift","stage"=>"Pop Stage","day"=>"Sunday","image"=>"../assets/images/taylor_swift.png","hits"=>["Love Story","Blank Space","Shake It Off"]],
+ ["artist"=>"Ariana Grande","stage"=>"Main Stage","day"=>"Saturday","image"=>"../assets/images/ariana_grande.png","hits"=>["7 rings","Into You","No Tears Left To Cry"]],
+["artist"=>"Dafina Zeqiri","stage"=>"Pop Stage","day"=>"Friday","image"=>"../assets/images/dafina_zeqiri.png","hits"=>["Four Seasons","Million Dollar Baby","Bye Bye"]],
+
+["artist"=>"Ledri Vula","stage"=>"Main Stage","day"=>"Saturday","image"=>"../assets/images/ledri_vula.png","hits"=>["Piano Rap","Krejt Shokët","Princess Diana"]],["artist"=>"Noizy","stage"=>"Main Stage","day"=>"Friday","image"=>"../assets/images/noizy.png","hits"=>["Toto","Jena Mbretër","Big Body Benzo"]],
+
+                                                                                                                                                                  ["artist"=>"Tayna","stage"=>"Pop Stage","day"=>"Saturday","image"=>"../assets/images/tayna.png","hits"=>["Columbiana","Sicko","Si ai"]],
+
+                                                                                                                                                                  ["artist"=>"Mozzik","stage"=>"Main Stage","day"=>"Sunday","image"=>"../assets/images/mozzik.png","hits"=>["Madonna","Bonjour Madame","Hana"]],
+
+                                                                                                                                                                  ["artist"=>"Yll Limani","stage"=>"Acoustic Stage","day"=>"Friday","image"=>"../assets/images/yll_limani.png","hits"=>["Buzeqeshja","Harrom","Nuk po du me t'pa"]]];
 
 /* =========================
    GET VALUES
