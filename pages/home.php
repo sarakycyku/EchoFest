3cdd9a8be99a91e742271a6fa4672d75b551a14b
Rrezon Ibishi <rrezon.ibishi@gmail.com>
2026-04-25 02:13:37 +0200

    initial html structure

--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,12 @@
-<?php
-include '../includes/header.php';
-// include '../includes/footer.php';
-
-?>
-
-
-    <?php include '../includes/footer.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>EchoFest 2026</title>
+  <link rel="stylesheet" href="../assets/css/style.css" />
+  <link rel="stylesheet" href="../assets/css/home.css" />
+</head>
+<body>
+</body>
+</html>
