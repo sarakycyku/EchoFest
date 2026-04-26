1c79cbe23af052f9e31555cd70b6a0948aaaf9a3
Rumesa <rumesa2005@gmail.com>
2026-04-27 01:11:48 +0200

    added admin.php liddhjen me logic dhe headerin

---
+++ pages/admin.php
@@ -0,0 +1,16 @@
+<?php require '../logic/admin.php'; ?>
+<?php require '../includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EchoFest | Master Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<link rel="stylesheet" href="/EchoFest/assets/css/admin.css">
+</head>
+<body>
+
+?>
