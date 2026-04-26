e441281ae56960724087c0c864a158464aa272fc
Rumesa <rumesa2005@gmail.com>
2026-04-27 01:06:27 +0200

    added datas to admin logic

---
+++ logic/admin.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+$DATA_DIR = __DIR__ . '/../data/';
+$LINEUP_FILE = $DATA_DIR . 'lineup_data.json';
+$EVENTS_FILE = $DATA_DIR . 'events_data.json';
+$USERS_FILE  = $DATA_DIR . 'users.json';
+$ORDERS_FILE = $DATA_DIR . 'orders.txt';
+?>
