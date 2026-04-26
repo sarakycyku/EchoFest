c7874c6bd634e91d6fc9a34504cc1ce5fac592a1
Rumesa <rumesa2005@gmail.com>
2026-04-26 02:52:53 +0200

    javascript logic

--- pages/news.php
+++ pages/news.php
@@ -90,6 +90,29 @@ $news = [
 
   </div>
 </div>
+<script>
+function openNews(title, desc, full){
+  document.getElementById("modalTitle").innerText = title;
+  document.getElementById("modalDesc").innerText = desc;
+  document.getElementById("modalFull").innerText = full;
+
+  document.getElementById("newsModal").style.display = "flex";
+  document.body.style.overflow = "hidden";
+}
+
+function closeNews(){
+  document.getElementById("newsModal").style.display = "none";
+  document.body.style.overflow = "auto";
+}
+
+window.onclick = function(e){
+  if(e.target.id === "newsModal"){
+    closeNews();
+  }
+}
+</script>
+
+<?php include '../includes/footer.php'; ?>
 
 
 
