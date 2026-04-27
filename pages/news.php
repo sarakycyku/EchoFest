44dc52907de746a1c3dbf6fcaadc30a4d3c03137
DuaBilalli <duabilalli13@gmail.com>
2026-04-27 20:52:02 +0200

    Refactor news page structure and update CSS link

--- pages/news.php
+++ pages/news.php
@@ -63,18 +63,11 @@ $news = [
   ]
 ];
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>EchoFest</title>
 <link rel="icon" href="../assets/images/logo2-pabg.png">
 
-<link rel="stylesheet" href="../assets/css/news.css">
-</head>
+<link rel="stylesheet" href="../assets/css/news.css?v=1.1">
 
-<body class="news-page">
+<div class="news-page">
 
 <?php include '../includes/header.php'; ?>
 
