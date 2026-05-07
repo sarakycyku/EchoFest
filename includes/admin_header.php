8871ba05ba2febd1baffed8678be93ea33bca480
sarak1234 <sarakycyku123@gmail.com>
2026-05-08 00:37:58 +0200

    krijimi i admin header

---
+++ includes/admin_header.php
@@ -0,0 +1,22 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>EchoFest Admin</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@300;400;500;700&display=swap" rel="stylesheet">
+<link rel="icon" href="/EchoFest/assets/images/logo2-pabg.png">
+<link rel="stylesheet" href="/EchoFest/assets/css/h.css">
+<script src="/EchoFest/assets/js/header.js?v=1.1" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+</head>
+<body>
+
+
