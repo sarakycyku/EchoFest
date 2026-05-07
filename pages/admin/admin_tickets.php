e8f705ed12ab6b79076798dd111826e278ac56c2
DuaBilalli <duabilalli13@gmail.com>
2026-05-07 23:59:35 +0200

    krijimi i admin header

--- pages/admin/admin_tickets.php
+++ pages/admin/admin_tickets.php
@@ -8,3 +8,10 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
 $tickets = json_decode(file_get_contents('../data/tickets.json'), true);
 ?>
 <link rel="stylesheet" href="../assets/css/admin_tickets.css">
+
+<div class="admin-wrap">
+    <div class="admin-header">
+        <h1 class="admin-title">Manage Tickets</h1>
+        <p class="admin-sub">Add, enable, disable or delete ticket types</p>
+    </div>
+</div>
